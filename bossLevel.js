800d73de5f0473c15e5475b66adb24fbc859e0c6
UsagiUchiha <sailors129@gmail.com>
2020-09-26 22:34:24 -0500

    new boss script

--- bossLevel.js
+++ bossLevel.js
@@ -19,14 +19,15 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var player;
 
 function preload ()
 {
     this.load.image('environment', 'Assets/Boss/Bossbackground.png');
-    this.load.image('floor', 'Assets/Bossfloor.png');
-    this.load.image('corona', 'Assets/Boss/corona.png'); 
+    this.load.image('floor', 'Assets/Boss/Bossfloor.png');
+    this.load.image('corona', 'Assets/Boss/corona.png');
     this.load.image('platform', 'Assets/Boss/platform.png');
-    this.load.spritesheet('player', 
+    this.load.spritesheet('whiteBC',
         'Assets/Players/whiteBC.png',
         { frameWidth: 55, frameHeight: 84 }
 );
@@ -35,15 +36,42 @@ function preload ()
 
 function create ()
 {
-   this.add.image(0, 0, 'environment') 
+   this.add.image(0, 0, 'environment')
+   cursors = this.input.keyboard.createCursorKeys();
+   player = this.physics.add.sprite(100, 450, "whiteBC");
+   playerCreation();
 }
 
-cursors = this.input.keyboard.createCursorKeys();
+function playerCreation(){
+    //Edge colliders
+    // this.playerCollider = this.player.setCollideWorldBounds(true);
+    // this.physics.add.collider(player, this.platforms);
+  this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("whiteBC", { start: 0, end: 0}),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [ { key: "whiteBC", frame: 0 } ],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("whiteBC", { start: 0, end: 7 }),
+      frameRate: 10,
+      repeat: -1
+    });
+  }
+
 function update()
 {
     if (cursors.left.isDown)
     {
-        player.setVelocityX(-160);
+        this.player.setVelocityX(-160);
 
         player.anims.play('left', true);
     }
