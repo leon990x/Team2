809127e46a58684715e2d97742201176bca0b143
UsagiUchiha <sailors129@gmail.com>
2020-09-26 23:54:43 -0500

    new bossLVL script

--- bossLevel.js
+++ bossLevel.js
@@ -6,8 +6,8 @@ bossScene = {
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     physics: {
         default: 'arcade',
         arcade: {
@@ -20,6 +20,7 @@ var config = {
 
 var game = new Phaser.Game(config);
 var player;
+var ground;
 
 function preload ()
 {
@@ -36,42 +37,52 @@ function preload ()
 
 function create ()
 {
-   this.add.image(0, 0, 'environment')
+   background = this.add.image(960, 540, 'environment');
+   boss = this.add.image(780, 480, "corona");
    cursors = this.input.keyboard.createCursorKeys();
-   player = this.physics.add.sprite(100, 450, "whiteBC");
-   playerCreation();
+
+
+   //Edge colliders
+   ground = this.physics.add.staticGroup();
+   ground.create(960, 950, "floor").setScale(2).refreshBody();
+   ground.create(400, 650, 'platform');
+   ground.create(800, 550, 'platform');
+   ground.create(1200, 650, 'platform');
+
+   // player code
+   player = this.physics.add.sprite(100, 700, "whiteBC");
+   player.setBounce(0.3);
+   player.setCollideWorldBounds(true);
+   this.physics.add.collider(player, ground);
+   player.body.setGravityY(1);
+
+   this.anims.create({
+       key: "left",
+       frames: this.anims.generateFrameNumbers("whiteBC", { start: 0, end: 0}),
+       frameRate: 10,
+       repeat: -1
+     });
+
+     this.anims.create({
+       key: "turn",
+       frames: [ { key: "whiteBC", frame: 0 } ],
+       frameRate: 20
+     });
+
+     this.anims.create({
+       key: "right",
+       frames: this.anims.generateFrameNumbers("whiteBC", { start: 0, end: 7 }),
+       frameRate: 10,
+       repeat: -1
+     });
 }
 
-function playerCreation(){
-    //Edge colliders
-    // this.playerCollider = this.player.setCollideWorldBounds(true);
-    // this.physics.add.collider(player, this.platforms);
-  this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("whiteBC", { start: 0, end: 0}),
-      frameRate: 10,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: "turn",
-      frames: [ { key: "whiteBC", frame: 0 } ],
-      frameRate: 20
-    });
-
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("whiteBC", { start: 0, end: 7 }),
-      frameRate: 10,
-      repeat: -1
-    });
-  }
 
 function update()
 {
     if (cursors.left.isDown)
     {
-        this.player.setVelocityX(-160);
+        player.setVelocityX(-160);
 
         player.anims.play('left', true);
     }
